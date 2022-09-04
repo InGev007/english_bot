d53bd36a668f241ac10c50e5cf7eebb351b5f75c
InGev <ingev123@gmail.com>
2022-09-05 01:10:03 +0300

    Изменил механику подбора слов

--- func.py
+++ func.py
@@ -32,7 +32,8 @@ def setstate(uid,state):
 def randansw():
     con = sqlite3.connect("bot.db")
     cur = con.cursor()
-    i1=(random.randint(0, 1000),random.randint(0, 1000),random.randint(0, 1000))
+    i1=list(range(1, 1000))
+    random.shuffle(i1)
     res = con.execute('SELECT ru FROM dictionary WHERE id in (%s,%s,%s)'% (i1[0],i1[1],i1[2]))
     res=res.fetchall()
     answ=[res[0][0],res[1][0],res[2][0]]
@@ -42,62 +43,66 @@ def randansw():
 def startlearn(uid):
     con = sqlite3.connect("bot.db")
     cur = con.cursor()
-    res = con.execute('SELECT COUNT(idu) FROM tempdict WHERE idu=%s;'% uid)
-    res=res.fetchone()
-    con.commit()
-    colln=res[0]+1
-    i=0
-    while i<50:
-        res = con.execute('INSERT INTO tempdict (idu,idd) VALUES (%s,%s);'% (uid,colln+i))
-        con.commit()
-        i+=1
+    res = con.execute('SELECT id FROM dictionary WHERE id NOT IN (SELECT idd FROM tempdict WHERE idu=%s);'% uid)
+    res=res.fetchall()
+    coll=len(res)
+    answ=res
+    random.shuffle(answ)
+    if coll!=0:
+        i=0
+        while i<50:
+            try:
+                res = con.execute('INSERT INTO tempdict (idu,idd) VALUES (%s,%s);'% (uid,answ[i][0]))
+                con.commit()
+            except:
+                pass
+            i+=1
     answ=randansw()
-    res = con.execute('SELECT en,ru FROM dictionary WHERE id=%s;'% colln)
-    res=res.fetchone()
-    answ.append(res[1])
-    sql = con.execute('INSERT INTO t_dial (idu,ganswer) VALUES (%s,"%s");'% (uid,answ[3]))
+    res = con.execute('SELECT en,ru,id FROM dictionary WHERE id IN (SELECT idd FROM tempdict WHERE idu=%s);'% uid)
+    res=res.fetchall()
+    random.shuffle(res)
+    answ.append(res[0][1])
+    sql = con.execute('INSERT INTO t_dial (idu,ganswer,idd) VALUES (%s,"%s",%s);'% (uid,answ[3],res[0][2]))
     con.commit()
     random.shuffle(answ)
-    answ.append(res[0])
+    answ.append(res[0][0])
     con.close()
     return answ
 
 def nextlearn(uid, text):
     con = sqlite3.connect("bot.db")
     cur = con.cursor()
-    res = con.execute('SELECT ganswer FROM t_dial WHERE idu=%s;'% uid)
+    res = con.execute('SELECT ganswer,idd FROM t_dial WHERE idu=%s;'% uid)
     res=res.fetchone()
-    answ=res[0]
+    answ=res
     res = con.execute('DELETE FROM t_dial WHERE idu=%s;'% uid)
     con.commit()
-    res= con.execute('SELECT id FROM dictionary WHERE ru="%s";'% answ)
-    res=res.fetchone()
-    idd=res[0]
-    if answ==text:
-        res = con.execute('UPDATE tempdict SET good=good+1,wrong=0 WHERE idd=%s AND idu=%s;'% (idd,uid))
+    idd=answ[1]
+    if answ[0]==text:
+        res = con.execute('UPDATE tempdict SET good=good+1, wrong=0, ok= CASE WHEN (ok=0 AND good = 4) OR (ok=1) THEN 1 ELSE 0 END WHERE idd=%s AND idu=%s;'% (idd,uid))
         ret="Правильный ответ. Следующие слово: "
     else:
         res = con.execute('UPDATE tempdict SET good=0,wrong=1 WHERE idd=%s AND idu=%s;'% (idd,uid))
         ret = "Неправильно. Правильный ответ: " + answ + ". Следующие слово: "
     con.commit()
-    res = con.execute('SELECT idd FROM tempdict WHERE idu=%s AND good<5 ORDER BY good'%uid)
+    res = con.execute('SELECT idd FROM tempdict WHERE idu=%s AND ((good<5 AND ok=0)OR(good<2 AND ok=1)) ORDER BY good;'%uid)
     res=res.fetchall()
     tempdict=res
     coll=len(tempdict)
     if coll==0:
-        setstate(idu,0)
-        res = con.execute('UPDATE tempdict SET good=0, wrong=0, ok=0 WHERE idu=%s'%uid)
+        setstate(uid,0)
+        res = con.execute('UPDATE tempdict SET good=0, wrong=0, ok=1 WHERE idu=%s'%uid)
         ret = "Ты успешно выучил ещё 50 слов. Продолжай в том-же духе и помни перерыв в обучении сведёт весь успех к нулю."
         con.commit()
         con.close()
         return ret,0
     else:
         random.shuffle(tempdict)
-        res = con.execute('SELECT en,ru FROM dictionary WHERE id=%s;'% tempdict[0][0])
+        res = con.execute('SELECT en,ru,id FROM dictionary WHERE id=%s;'% tempdict[0][0])
         res=res.fetchone()
         answ=randansw()
         answ.append(res[1])
-        sql = con.execute('INSERT INTO t_dial (idu,ganswer) VALUES (%s,"%s");'% (uid,answ[3]))
+        sql = con.execute('INSERT INTO t_dial (idu,ganswer,idd) VALUES (%s,"%s",%s);'% (uid,answ[3],res[2]))
         con.commit()
         random.shuffle(answ)
         con.close()
@@ -109,6 +114,8 @@ def undolearn(uid):
     cur = con.cursor()
     res = con.execute('DELETE FROM t_dial WHERE idu=%s;'% uid)
     res = con.execute('DELETE FROM tempdict WHERE idu=%s AND ok=0;'% uid)
+    con.commit()
+    res = con.execute('UPDATE tempdict SET good=0, wrong=0 WHERE idu=%s'%uid)
     con.commit()
     con.close()
     return
--- test.py
+++ test.py
@@ -2,9 +2,9 @@ import random
 import sqlite3
 con = sqlite3.connect("bot.db")
 cur = con.cursor()
-res = con.execute('SELECT idd FROM tempdict WHERE idu=0 AND good<5 ORDER BY good')
+res = con.execute('SELECT id FROM dictionary WHERE id NOT IN (SELECT idd FROM tempdict WHERE idu=786607486);')
 res=res.fetchall()
 con.commit()
-coll=len(res)
-#random.shuffle(res)
-print(coll)
+random.shuffle(res)
+print(res[0][0])
+con.close()

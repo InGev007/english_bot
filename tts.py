8ead17d0958ab01b2522dc03fd7d9f13f5904080
InGev007 <101068090+InGev007@users.noreply.github.com>
2022-09-20 10:50:43 +0300

    Update tts.py
    fix

--- tts.py
+++ tts.py
@@ -15,7 +15,6 @@ async def checkandupdatevoice(bot, idu):
       idd = word[0]
       language = 'en'
       myobj = gTTS(text=mytext, lang=language, slow=False)
-      myobj.save("./speech/%s.mp3"%idd)  
       try:
           myobj.save("./speech/%s.mp3"%idd)
       except:

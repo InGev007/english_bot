af754d6d7edcf04b87d54f5159b764f012fc6733
InGev007 <101068090+InGev007@users.noreply.github.com>
2022-09-20 10:24:48 +0300

    Update tts.py

--- tts.py
+++ tts.py
@@ -1,12 +1,13 @@
 import sqlite3
 from gtts import gTTS
 import os
-from aiogram.types import FSInputFile
+from aiogram import Bot, types
+from aiogram.types import InputFile
 
-def checkandupdatevoice():
+async def checkandupdatevoice(bot, idu):
   con = sqlite3.connect("./db/bot.db")
   cur = con.cursor()
-  res = cur.execute('SELECT id, en FROM dictionary WHERE voice is NULL')
+  res = cur.execute('SELECT id, en FROM dictionary WHERE voice is NULL LIMIT 2')
   res = res.fetchall()
   if len(res)!=0:
     for word in row:
@@ -21,7 +22,10 @@ def checkandupdatevoice():
           os.mkdir('speech')
       finally:
           myobj.save("./speech/%s.mp3"%idd)
-      voice_id = FSInputFile("./speech/%s.mp3"%(id,mytext.replace("'", "")))
+      #voice = InputFile("./speech/%s.mp3"%mytext.replace("'", ""))
+      voice = InputFile("./speech/%s.mp3"%idd)
+      voice_send = await bot.send_audio(chat_id=idu, audio=voice, caption="текст1",title=mytext) # этот метод поможет получить file_id
+      voice_id = voice_send['audio'][0]['file_id'] # это сам file_id
       res = cur.execute('UPDATE dictionary SET voice=%s WHERE id=%s;'% (voice_id,idd))
       con.commit()
   con.close()
